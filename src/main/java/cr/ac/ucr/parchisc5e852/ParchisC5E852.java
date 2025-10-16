d2d4ac0fe28e64be53a0ad06f49447a6dce4ff22
Jean Escobar <JEAN.ESCOBARROJAS@ucr.ac.cr>
2025-10-16 14:16:37 -0600

    Primer avance 16/10/25

---
+++ src/main/java/cr/ac/ucr/parchisc5e852/ParchisC5E852.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cr.ac.ucr.parchisc5e852;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class ParchisC5E852 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
